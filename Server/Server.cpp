a1ad7d839d992e7fd2768b20b8ad6f253289c9e5
The3ven <the3ven@gmail.com>
2024-01-14 13:04:36 +0530

    Devide Client & Server to thair own folders & make websocket server to pointer so it should run on gateway ip in this way clients can easily make request of connection on gateway with port
    Signed-off-by: The3ven <the3ven@gmail.com>

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,7 +1,7 @@
 #include "Server.h"
 
 Wifi_AP wap;
-WS_SERVER ws_server = WS_SERVER(8889);
+WS_SERVER* ws_server;
 String tmp_str;
 String strp[10];
 UTILS util;
@@ -12,23 +12,25 @@ void Serversetup()
     wap.setPassKey("12345678");
     if (wap.make_softAP(15))
     {
-        while(!wap.isSoftAP());
-        ws_server.begin();
+        while (!wap.isSoftAP());
+        // WS_SERVER ws_server = WS_SERVER(8889);
+        ws_server = new WS_SERVER(8889);
+        ws_server->begin();
     }
 }
 
 void Serverloop()
 {
-    ws_server.loop();
+    ws_server->loop();
     delay(100);
-    String WS_recivedString = ws_server.getRecivedmsg();
+    String WS_recivedString = ws_server->getRecivedmsg();
     if (WS_recivedString.length() > 0)
     {
         Serial.println(WS_recivedString);
-        util.splitstr(WS_recivedString,"@",strp);
+        util.splitstr(WS_recivedString, "@", strp);
         int remote_add = String(strp[0]).toInt();
-        ws_server.sendMsg(remote_add,"ok");
-        String Clint_IP = ws_server.remoteIP(remote_add).toString();
+        ws_server->sendMsg(remote_add, "ok");
+        String Clint_IP = ws_server->remoteIP(remote_add).toString();
         Serial.println("Server Messege is : " + strp[1]);
         Serial.println("Server IP is : " + Clint_IP);
         util.Parser(WS_recivedString);
